a38da1a9000ac46b17910d5e715ad05f46ee059d
Lishenko Vitaliy <lishenkovitaliy@gmail.com>
2016-08-30 08:48:18 +0300

    Finish all tests

--- js/common.js
+++ js/common.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-	$('.slider-wrap').slick({
+  
+  //Language menu
+  $(document).mouseup(function (e){
+      var div = $(".languale-list");
+      var div2 = $("#language_btn");
+      
+      if (
+          !div.is(e.target)
+          && !div2.is(e.target)
+          && div.has(e.target).length === 0
+          && div2.has(e.target).length === 0)
+          { 
+          div.addClass("hidden");
+      }
+      else {
+         div.removeClass("hidden");
+      }
+    });
+
+
+	//Slider
+  $('.slider-wrap').slick({
 	  slidesToShow: 3,
 	  slidesToScroll: 1,
-	  // autoplay: true,
-	  // autoplaySpeed: 2000,
+	  autoplay: true,
+	  autoplaySpeed: 2000,
 	  responsive: [
     {
       breakpoint: 800,
@@ -15,4 +36,12 @@ $(document).ready(function() {
     },
   ]
 	});
+
+  //Pop-up Validation
+  $(".slider-item a").magnificPopup({type:"inline", midClick: true});
+
+  //Pop-up terms
+  $(".footer-terms a").magnificPopup({type:"inline", midClick: true});
+
+
 });
